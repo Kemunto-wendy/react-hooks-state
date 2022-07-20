23c725615820a18de62b652db2f7a8c32f0cd3d3
JOY <joy.kemunto@student.moringaschool.com>
2022-07-20 14:13:34 +0300

    commit tree 0a89edd5307def2033cd90cc7b01753e44e9a45b

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,10 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(count);
+    setCount(count + 1);
+
+    console.log(count);
     setCount(count + 1);
   }
 
